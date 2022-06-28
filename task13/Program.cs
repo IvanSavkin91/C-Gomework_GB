e319c74b5df8373299b057c9365366f3223bb8e5
IvanSavkin <adoys1@gmail.com>
2022-06-29 01:25:56 +0300

    homework2

---
+++ task13/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+645 -> 5
+
+78 -> третьей цифры нет
+
+32679 -> 6
+*/
+Random rand = new Random();
+Console.WriteLine("Введите число ");
+string number = Console.ReadLine();
+void Task13(string number )
+{
+    if (number.Length < 3)
+    {
+    Console.WriteLine("Третьей цифры нет");
+    }
+    else
+Console.WriteLine(number [2]);
+}
+Task13(number);
---
+++ task15/Program.cs
@@ -0,0 +1,29 @@
+﻿/*Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, 
+является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет
+*/
+
+Console.WriteLine("введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+ if (number == 1)
+Console.Write ("Нет. Понедельни рабочий день.");
+    else if (number == 2)
+Console.Write ("Нет. Вторник рабочий день.");
+   else if (number == 3)
+Console.Write ("Нет. Среда рабочий день.");
+   else if (number == 4)
+Console.Write ("Нет. Четвег рабочий день.");
+   else if (number == 5)
+Console.Write ("Нет. Пятница рабочий день.");
+   else if (number == 6)
+Console.Write ("Да. Суббота выходной день.");
+   else if (number == 7)
+Console.Write ("Да. Воскресение выходной день.");
+
+    else 
+{
+Console.Write ("Введите число соответствующее дню недели (от 1 до 7).");
+}
